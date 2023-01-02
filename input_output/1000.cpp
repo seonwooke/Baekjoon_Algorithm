ab2745d08b4d081f2526c0a3cf4669585449f1f0
seonwooke <seonookkim@gmail.com>
2023-01-02 16:52:40 +0900

    :bulb: 1000
    Complete

---
+++ input_output/1000.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int A, B ;
+	cin >> A >> B ;
+
+	if ((A <= 0) || (B >= 10))
+		return 1 ;
+
+	int result = A + B ;
+	cout << result << endl ;
+
+	return 0 ;
+}
