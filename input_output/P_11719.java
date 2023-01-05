90f7ff8c4a890719cad710f4ec5c0ce11a51bd00
seonwooke <seonookkim@gmail.com>
2023-01-06 00:26:43 +0900

    :bulb: 11719
    Complete

---
+++ input_output/P_11719.java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+
+public class P_11719 {
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in) ;
+
+		while (sc.hasNextLine()) {
+			String input = sc.nextLine() ;
+			System.out.println(input) ;
+		}
+
+		sc.close() ;
+	}
+}
