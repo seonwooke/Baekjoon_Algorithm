b9156f91d15b816cd3e11f982a93af9c6069bf9c
seonwooke <seonookkim@gmail.com>
2023-01-16 13:47:24 +0900

    :bulb: 9095
    Ing

---
+++ dynamic_programming/P_9095.java
@@ -0,0 +1,12 @@
+package dynamic_programming ;
+
+import java.io.BufferedReader ;
+import java.io.IOException ;
+import java.io.InputStreamReader ;
+
+public class P_9095 {
+    public static void main (String[] args) throws IOException {
+
+        System.exit(0) ;
+    }
+}
