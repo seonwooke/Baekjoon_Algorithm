77eea25047a6c616f464422115db929cf7f6f526
seonwooke <seonookkim@gmail.com>
2023-01-17 12:28:07 +0900

    :bulb: 9095
    Ing

--- dynamic_programming/P_9095.java
+++ dynamic_programming/P_9095.java
@@ -7,6 +7,10 @@ import java.io.InputStreamReader ;
 public class P_9095 {
     public static void main (String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
+        int N = Integer.parseInt(br.readLine()) ;
+
+        br.close() ;
         System.exit(0) ;
     }
 }
