a457672a375b4bdef71f48ab7c55b4032f8d9c9d
seonwooke <seonookkim@gmail.com>
2023-02-28 14:33:44 +0900

    :bulb: 10844
    Ing

---
+++ dynamic_programming/P_10844.java
@@ -0,0 +1,18 @@
+package dynamic_programming ;
+
+import java.io.BufferedReader ;
+import java.io.IOException ;
+import java.io.InputStreamReader ;
+
+public class P_10844 {
+    public static void main (String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)) ;
+
+        
+
+        br.close() ;
+        System.exit(0) ;
+
+    }
+}
